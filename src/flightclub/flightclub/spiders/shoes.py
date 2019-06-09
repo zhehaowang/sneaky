21852c119c208f7c937e96be5b0f0d26c6de4353
zhehaowang <wangzhehao410305@gmail.com>
2019-06-08 20:58:45 -0400

    flightclub: correct xpath; remove unused

--- src/flightclub/flightclub/spiders/shoes.py
+++ src/flightclub/flightclub/spiders/shoes.py
@@ -5,7 +5,7 @@ from scrapy import Request, signals
 from scrapy.xlib.pydispatch import dispatcher
 
 import json
-import date
+import datetime
 
 class ShoesSpider(scrapy.Spider):
     name = 'shoes'
@@ -24,37 +24,49 @@ class ShoesSpider(scrapy.Spider):
         content_table = selector.xpath("//li[contains(@class, 'level1')]//@href").getall()
         for url in content_table:
             # print(item)
-            yield Request(url, headers={'User-Agent':'Mozilla/5.0'}, callback=self.parse_items_page)
+            if 'jordan' in url or 'nike' in url or 'adidas' in url or 'yeezy' in url:
+                yield Request(url, headers={'User-Agent':'Mozilla/5.0'}, callback=self.parse_items_page)
 
     def parse_items_page(self, response):
         # second level menu
         selector = Selector(response)
         content_table = selector.xpath("//li[contains(@class, 'item')]//@href").getall()
         for url in content_table:
-            yield Request(url, headers={'User-Agent':'Mozilla/5.0'}, callback=lambda r: self.parse_single_model_page(r, url))
+            yield Request(url, headers={'User-Agent':'Mozilla/5.0'}, callback=lambda r, url=url: self.parse_single_model_page(r, url))
 
     def parse_single_model_page(self, response, url):
         # single model page
         selector = Selector(response)
-        size_buttons = selector.xpath("//button[contains(@class, 'attribute-button-text')]/text()").getall()
+        size_buttons = selector.xpath("//button[contains(@class, 'attribute-button-text') and contains(@id, 'jpi_option') and not(contains(@id, 'for-notification'))]/text()").getall()
         for btn in size_buttons:
             try:
                 shoe_size = float(btn.strip())
-                yield Request(url + '?size=' + str(shoe_size), headers={'User-Agent':'Mozilla/5.0'}, callback=lambda r: self.get_price_of_model_size(r, shoe_size))
+                print(url, shoe_size)
+                yield Request(url + '?size=' + str(shoe_size), headers={'User-Agent':'Mozilla/5.0'}, callback=lambda r, shoe_size=shoe_size, url=url: self.get_price_of_model_size(r, shoe_size, url))
             except:
                 # swallow
                 continue
 
-    def get_price_of_model_size(self, response, shoe_size):
+    def get_price_of_model_size(self, response, shoe_size, url):
         selector = Selector(response)
-        price_elems = selector.xpath("//span[@class='price']/text()").getall()
+        price_elems = selector.xpath("//span[contains(@id, 'current-price')]/span/text()").getall()
         brand_elems = selector.xpath("//h2[@itemprop='brand']/text()").getall()
         name_elems = selector.xpath("//h1[@itemprop='name']/text()").getall()
-        if len(price_elems) > 0:
+        
+        px = None
+        if len(price_elems) == 1:
             px = price_elems[0].strip().strip('$')
-            if len(brand_elems) > 0 and len(name_elems) > 0:
-                shoe_full_name = brand_elems[0].strip() + ' -- ' + name_elems[0].strip() + ' -- ' + str(shoe_size)
-                self.prices[shoe_full_name] = px
+        else:
+            for elem in price_elems:
+                if not px:
+                    px = elem.strip().strip('$')
+                elif px != elem.strip().strip('$'):
+                    print('cannot determine px for {}'.format(url))
+
+        if len(brand_elems) > 0 and len(name_elems) > 0 and px:
+            shoe_full_name = brand_elems[0].strip() + ' -- ' + name_elems[0].strip() + ' -- ' + str(shoe_size)
+            self.prices[shoe_full_name] = {"px": px, "url": url}
+            print(shoe_full_name, px)
 
     def closed(self, reason):
         with open("flightclub.{}.txt".format(datetime.date.today().strftime("%Y%m%d")), "w") as wfile:
