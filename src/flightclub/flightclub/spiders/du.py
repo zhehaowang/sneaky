1fc8a1d2615506ef3b394d459531c4e91387ac45
zhehaowang <wangzhehao410305@gmail.com>
2019-06-23 21:40:26 -0400

    feed: clean up unused du functions, add last transactions query

--- src/flightclub/flightclub/spiders/du.py
+++ src/flightclub/flightclub/spiders/du.py
@@ -11,6 +11,7 @@ import hashlib
 import pprint
 import urllib
 import execjs
+import re
 
 pp = pprint.PrettyPrinter()
 
@@ -45,7 +46,10 @@ class DuSpider(scrapy.Spider):
         self.cookie = None
         self.js_ctx = None
         self.prices = {}
-        self.max_pages = 400
+        self.max_pages = 2
+
+        self.max_history_page = 3
+        self.history_queries = {}
 
         if os.path.isfile(DuSpider.token_file):
             with open(DuSpider.token_file, 'r') as rfile:
@@ -96,9 +100,8 @@ class DuSpider(scrapy.Spider):
 
     def parse_login(self, response):
         """
-        Top-level menu on homepage, filter links by known brands.
-        Send request for each (brand, series), e.g.
-        `https://www.flightclub.com/air-jordans/air-jordan-2`
+        we can log in with this, but later steps doesn't really require logging
+        in
         """
         if response.status != 200:
             print("login http request failed: {}".format(response.status))
@@ -136,39 +139,22 @@ class DuSpider(scrapy.Spider):
             except KeyError as e:
                 print("unexpected key {}".format(e))
 
-    def get_sign(self, api_params):
-        hash_map = {
-            "uuid": self.headers["uuid"],
-            "platform": self.headers["platform"],
-            "v": self.headers["v"],
-            "loginToken": self.du_token,
-        }
+    def get_url(self, target, params):
+        if not self.js_ctx:
+            with open('sign.js', 'r', encoding='utf-8')as f:
+                all_ = f.read()
+                self.js_ctx = execjs.compile(all_)
 
-        for k in api_params:
-            hash_map[k] = api_params[k]
+        sign_query = ''
+        url_get = ''
+        for key in sorted(params.keys()):
+            sign_query += key + params[key]
+            url_get += key + '=' + params[key] + '&'
 
-        hash_map = sorted(hash_map.items(), key=lambda x: x[0])
+        sign_query += '048a9c4943398714b356a696503d2d36'
+        sign = self.js_ctx.call('getSign', sign_query)
 
-        result = ''
-        for v in hash_map:
-            result += v[0] + v[1]
-
-        result += "重要参数用于接口sign加密。"
-
-        m1 = hashlib.md5()
-        m1.update(result.encode("GBK"))
-        sign = m1.hexdigest()
-        return sign
-
-    def get_api_url(self, api_url, api_params):
-        url = DuSpider.base_url
-        url += api_url
-        url += '?'
-        for k in api_params:
-            url += k + '=' + api_params[k] + '&'
-        # 获取sign
-        sign = self.get_sign(api_params)
-        url += 'sign=' + sign
+        url = 'https://m.poizon.com/mapi/' + target + '?{}sign={}'.format(url_get, sign)
         return url
 
     def get_list_request(self, page_id):
@@ -181,9 +167,9 @@ class DuSpider(scrapy.Spider):
             "sortType": "0",
             "sortMode": "1",
             "page": str(page_id),
-            "limit": "20",
+            "limit": "20"
         }
-        url = self.get_api_url('/search/list', params)
+        url = self.get_url('search/list', params)
         return Request(
             url, headers=self.headers, callback=self.parse_list)
 
@@ -200,57 +186,20 @@ class DuSpider(scrapy.Spider):
                     print('retrieved {} items'.format(len(product_list)))
                     for product in product_list:
                         product_id = product['product']['productId']
-                        yield self.get_details(product_id)
+                        params = {
+                            'source': 'shareDetail',
+                            'productId': str(product_id)
+                        }
+                        product_id_url = self.get_url('product/detail', params)
+                        yield Request(
+                            product_id_url,
+                            callback=lambda r, product_id=product_id, url=product_id_url: self.parse_item(r, product_id, product_id_url))
             except json.JSONDecodeError as e:
                 print(e)
                 print("unable to decode {}".format(response.body_as_unicode()))
             except KeyError as e:
                 print('unexpected parse_list response {}'.format(e))
         return
-
-    def get_details(self, product_id):
-        params = {
-            # fixed
-            'lastId': '',
-            'mode': '0',
-            'platform': 'iPhone',
-            'shumeiid': '201906191136236fded65cbbdfa090f71e09578a0feac101227030b29a0ef5',
-            'token': urllib.parse.quote_plus('JLIjsdLjfsdII%3D%7CMTQxODg3MDczNA%3D%3D%7C07aaal32795abdeff41cc9633329932195'),
-            'uuid': '1C6BD899-8A6A-4E9A-BCA8-9E6BA149D7A7',
-            'v': '4.2.1',
-
-            'productId': str(product_id),
-            'isChest': '1',
-            'loginToken': urllib.parse.quote_plus(self.du_token),
-
-            # may need to change
-            'timestamp': self.timetamp
-        }
-        url = self.get_api_url('/product/detail', params)
-        get_details_head = dict(self.headers)
-        get_details_head['token'] = 'JLIjsdLjfsdII%3D%7CMTQxODg3MDczNA%3D%3D%7C07aaal32795abdeff41cc9633329932195'
-        get_details_head['logintoken'] = self.du_token
-        get_details_head['shumeiid'] = '201906191136236fded65cbbdfa090f71e09578a0feac101227030b29a0ef5'
-        if 'content-type' in get_details_head:
-            del get_details_head['content-type']
-
-        product_id_url = self.get_product_url(product_id)
-
-        return Request(
-            product_id_url,
-            # cookies=self.cookie,
-            # headers=get_details_head,
-            callback=lambda r, product_id=product_id, url=product_id_url: self.parse_item(r, product_id, product_id_url))
-
-    def get_product_url(self, product_id):
-        if not self.js_ctx:
-            with open('sign.js', 'r', encoding='utf-8')as f:
-                all_ = f.read()
-                self.js_ctx = execjs.compile(all_)
-
-        sign = self.js_ctx.call('getSign', 'productId{}sourceshareDetail048a9c4943398714b356a696503d2d36'.format(product_id))
-        product_url = 'https://m.poizon.com/mapi/product/detail?productId={}&source=shareDetail&sign={}'.format(product_id, sign)
-        return product_url
 
     def parse_item(self, response, product_id, product_id_url):
         if response.status != 200:
@@ -287,11 +236,85 @@ class DuSpider(scrapy.Spider):
                             'color': color
                         }
 
+                    yield self.get_last_sold_items(style_id, product_id)
             except json.JSONDecodeError as e:
                 print(e)
                 print("unable to decode {}".format(response.body_as_unicode()))
             except KeyError as e:
                 print('unexpected parse_item response {}'.format(e))
+        return
+
+    def get_last_sold_items(self, style_id, product_id, last_id=None):
+        params = {
+            'limit': '20',
+            'productId': str(product_id)
+        }
+        if last_id:
+            params['lastId'] = last_id
+        url = self.get_url('product/lastSoldList', params)
+        return Request(url, callback=lambda r, style_id=style_id, product_id=product_id: self.parse_transactions(r, style_id, product_id))
+
+    def timestr_to_epoch(self, timestr, timenow=None):
+        time_now = timenow if timenow else datetime.datetime.now()
+        
+        match = re.match(r'([0-9]+)分钟前', timestr)
+        if match:
+            minutes = int(match.group(1))
+            return (time_now - datetime.timedelta(minutes=minutes)).isoformat()
+        
+        match = re.match(r'([0-9]+)小时前', timestr)
+        if match:
+            hours = int(match.group(1))
+            return (time_now - datetime.timedelta(hours=hours)).isoformat()
+        
+        match = re.match(r'([0-9]+)天前', timestr)
+        if match:
+            days = int(match.group(1))
+            return (time_now - datetime.timedelta(days=days)).isoformat()
+        
+        match = re.match(r'([0-9]+)月前', timestr)
+        if match:
+            months = int(match.group(1))
+            return (time_now - datetime.timedelta(days=months * 30)).isoformat()
+        
+        print('failed to parse timestr {}'.format(timestr))
+
+    def parse_transactions(self, response, style_id, product_id):
+        if response.status != 200:
+            print('http last_sold_list request failed')
+        else:
+            try:
+                resobj = json.loads(response.body_as_unicode())
+                if resobj['status'] != 200:
+                    print('app last_sold_list request failed: {}', response.body_as_unicode())
+                else:
+                    last_id = resobj['data']['lastId']
+                    transactions_list = resobj['data']['list']
+                    for transaction in transactions_list:
+                        size = transaction['item']['formatSize']
+                        if style_id not in self.prices or size not in self.prices[style_id]:
+                            print('cannot find item to attach transaction to: {} size {}'.format(style_id, size))
+                            continue
+                        transaction_time = self.timestr_to_epoch(transaction['formatTime'])
+                        if 'transactions' not in self.prices[style_id][size]:
+                            self.prices[style_id][size]['transactions'] = [{
+                                'time': transaction_time
+                            }]
+                        else:
+                            self.prices[style_id][size]['transactions'].append({
+                                'time': transaction_time
+                            })
+                    if product_id not in self.history_queries:
+                        self.history_queries[product_id] = 1
+                    else:
+                        self.history_queries[product_id] += 1
+                    if self.history_queries[product_id] < self.max_history_page:
+                        yield self.get_last_sold_items(style_id, product_id, last_id)
+            except json.JSONDecodeError as e:
+                print(e)
+                print("unable to decode {}".format(response.body_as_unicode()))
+            except KeyError as e:
+                print('unexpected parse_transactions response {}'.format(e))
         return
 
     def closed(self, reason):
